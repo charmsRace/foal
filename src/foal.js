8951163e9fd359af36a484fd066996ae77a83eab
Alex Ruble <alex.ruble@q-free.com>
2018-03-07 21:09:55 -0500

    concatenation of numerals is the most primitive operation (i.e. PEMDASC)

--- src/foal.js
+++ src/foal.js
@@ -4,14 +4,19 @@
 function id(x) { return x[0]; }
 
     const nuller = () => null;
+    const makeArray = d => [d];
+    const concatAll = d => d[0].join('');
+    const getByIndex = i => d => d[i];
+    // const id = getByIndex(0);
 var grammar = {
     Lexer: undefined,
     ParserRules: [
     {"name": "foal", "symbols": ["number"]},
     {"name": "number$ebnf$1", "symbols": []},
     {"name": "number$ebnf$1", "symbols": ["number$ebnf$1", "digit"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "number", "symbols": ["number$ebnf$1"]},
-    {"name": "digit", "symbols": ["_", /[0-9]/, "_"]},
+    {"name": "number", "symbols": ["number$ebnf$1"], "postprocess": concatAll},
+    {"name": "dee", "symbols": [/[dD]/]},
+    {"name": "digit", "symbols": ["_", /[0-9]/, "_"], "postprocess": getByIndex(1)},
     {"name": "_$ebnf$1", "symbols": []},
     {"name": "_$ebnf$1", "symbols": ["_$ebnf$1", /[ \t\n\v\f]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "_", "symbols": ["_$ebnf$1"], "postprocess": nuller}
--- src/foal.ne
+++ src/foal.ne
@@ -3,12 +3,19 @@
 
 @ {%
     const nuller = () => null;
+    const makeArray = d => [d];
+    const concatAll = d => d[0].join('');
+    const getByIndex = i => d => d[i];
+    // const id = getByIndex(0);
 %}
 
 foal -> number
 
-number -> digit:*
 
-digit -> _ [0-9] _
+number -> digit:* {% concatAll %}
+
+dee -> [dD]
+
+digit -> _ [0-9] _ {% getByIndex(1) %}
 
 _ -> [ \t\n\v\f]:* {% nuller %}
